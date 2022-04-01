5fc3dc4741e4bad51ce60d6accfc1a789fa669c9
smallpence <smallpence.uk@gmail.com>
2022-04-01 09:04:16 +0100

    added new commands made it use tts for now & moved to 2am

--- index.ts
+++ index.ts
@@ -4,13 +4,15 @@ import { schedule } from "node-cron";
 import { readFileSync, writeFileSync, existsSync } from "fs";
 import { sample } from "lodash"
 
-type Response =                "sleeping" | "under bed" | "jackson" | "poll" | "illegible" | "bHGVw" | "zXgy" | "bAeVN" | "toes" | "9am" | "funfact" | "eggman" | "rem" | "nightman" | "spotify" | "rember" |
+type Response =                "sleeping" | "under bed" | "jackson" | "poll" | "illegible" | "bHGVw" | "zXgy" | "bAeVN" | "toes" | "9am" | "funfact" | "eggman" | "rem" | "nightman" | "spotify" | "rember" | "bedoclock" | "monopoly" | "poem1" | "poem2" |
                                       "-15";
-const responses: Response[] = ["sleeping" , "under bed" , "jackson" , "poll" , "illegible" , "bHGVw" , "zXgy" , "bAeVN" , "toes" , "9am" , "funfact" , "eggman" , "rem" , "nightman" , "spotify" , "rember"];
+const responses: Response[] = ["sleeping" , "under bed" , "jackson" , "poll" , "illegible" , "bHGVw" , "zXgy" , "bAeVN" , "toes" , "9am" , "funfact" , "eggman" , "rem" , "nightman" , "spotify" , "rember" , "bedoclock" , "monopoly" , "poem1" , "poem2"];
 const mentionResponses: Response[] = ["-15"];
 
 const CHANNELS_PATH = "channels.json";
 const LAST_PATH = "last.json";
+
+const tts = true;
 
 interface ChannelToSend {
     guild: string
@@ -85,23 +87,23 @@ async function send2AM(guild: Guild, channel: TextChannel) {
         return await channel.send("<@175351872822050816> is sleeping in the Esports centre right now");
 
     if (response === "under bed")
-        return await channel.send("Check under your bed tonight!");
+        return await channel.send({content: "Check under your bed tonight!", tts: tts});
 
     if (response === "jackson")
         return await channel.send("https://www.youtube.com/watch?v=SDCqgHLX8Ys");
 
     if (response === "poll") {
-        const message = await channel.send("Are you asleep?  ☑️ Yes ❎ No");
+        const message = await channel.send({content: "Are you asleep?  ☑️ Yes ❎ No", tts: tts});
         await message.react("☑️");
         await message.react("❎");
         return message;
     }
 
     if (response === "illegible")
-        return await channel.send("现在是凌晨 2 点，你还没有睡着。光荣的总统会怎么想？");
+        return await channel.send({content: "现在是凌晨 2 点，你还没有睡着。光荣的总统会怎么想？", tts: tts});
 
     if (response === "-15")
-        return await channel.send(await mentionSomeoneInVC(guild, "{user}, we have noticed it is 2am and you are still gaming. -15 social credits."));
+        return await channel.send({content: await mentionSomeoneInVC(guild, "{user}, we have noticed it is 2am and you are still gaming. -15 social credits."), tts: tts});
 
     if (response === "bHGVw")
         return await channel.send("https://tenor.com/bHGVw.gif");
@@ -113,34 +115,90 @@ async function send2AM(guild: Guild, channel: TextChannel) {
         return await channel.send("https://tenor.com/bAeVN.gif");
 
     if (response === "toes")
-        return await channel.send("Get to bed or your toes will be gobbled");
+        return await channel.send({content: "Get to bed or your toes will be gobbled", tts: tts});
 
     if (response === "funfact")
-        return await channel.send("Fun Fact! Did you know that people who stay awake gaming are found to be less likely to be attractive to their preferred gender?");
+        return await channel.send({content: "Fun Fact! Did you know that people who stay awake gaming are found to be less likely to be attractive to their preferred gender?", tts: tts});
 
     if (response === "9am") {
         const hours = new Date().getHours();
 
         const hoursTill9AM = hours < 9 ? 9 - hours : 33 - hours
-        return await channel.send(`There are ${hoursTill9AM} hours until your 9am tomorrow :) Maybe get some sleep`);
+        return await channel.send({content: `There are ${hoursTill9AM} hours until your 9am tomorrow :) Maybe get some sleep`, tts: tts});
     }
 
     if (response === "eggman") {
-        await channel.send("I've come to make an announcement; The Sandman's a bitch ass motherfucker, he dream cucked my fucking wife. Thats right, he took his Scandinavian bag of sand out and he threw sleep sand at my fucking wife, and he said his dreams were \"This long\" and I said that's disgusting, so I'm making a callout post on my twitter dot com, Sandman, you've got short dreams, it's the length of a Compsci students sleep except WAY smaller, and guess what? Here's what my dreams look like: PFFFT, THAT'S RIGHT, BABY. ALL FUN, NO NIGHTMARES, ALOTTA PILLOWS. Look at that, it looks like Inception, but the lead wouldt date 2nd years. He dream cucked wife so guess what? I'm gonna put the Earth to sleep. THAT'S RIGHT THIS IS WHAT YOU GET, MY SUPER LASER SLEEP! Except I'm not gonna put the Earth to Sleep. I'm gonna go higher. I'M DOZING OFF ON THE MOON! HOW DO YOU LIKE THAT, METALLICA? I PUT THE MOON TO SLEEP YOU IDIOT! YOU HAVE 23 HOURS BEFORE THE SLEEP DROPLETS HIT THE FUCKING EARTH NOW GET OUT OF MY SIGHT BEFORE I SEND YOU TO DREAMLAND TOO.");
+        await channel.send({content: "I've come to make an announcement; The Sandman's a bitch ass motherfucker, he dream cucked my fucking wife. Thats right, he took his Scandinavian bag of sand out and he threw sleep sand at my fucking wife, and he said his dreams were \"This long\" and I said that's disgusting, so I'm making a callout post on my twitter dot com, Sandman, you've got short dreams, it's the length of a Compsci students sleep except WAY smaller, and guess what? Here's what my dreams look like: PFFFT, THAT'S RIGHT, BABY. ALL FUN, NO NIGHTMARES, ALOTTA PILLOWS. Look at that, it looks like Inception, but the lead wouldt date 2nd years. He dream cucked wife so guess what? I'm gonna put the Earth to sleep. THAT'S RIGHT THIS IS WHAT YOU GET, MY SUPER LASER SLEEP! Except I'm not gonna put the Earth to Sleep. I'm gonna go higher. I'M DOZING OFF ON THE MOON! HOW DO YOU LIKE THAT, METALLICA? I PUT THE MOON TO SLEEP YOU IDIOT! YOU HAVE 23 HOURS BEFORE THE SLEEP DROPLETS HIT THE FUCKING EARTH NOW GET OUT OF MY SIGHT BEFORE I SEND YOU TO DREAMLAND TOO.", tts: tts});
         return await channel.send("https://tenor.com/bhOMg.gif");
     }
 
     if (response === "nightman") 
-        return await channel.send("The Nightman Cometh.....");
+        return await channel.send({content: "The Nightman Cometh.....", tts: tts});
 
     if (response === "rem") 
         return await channel.send("https://cdn.discordapp.com/attachments/958369561307131965/959143838163804190/Screenshot_2022-03-31_183500.png");
 
     if (response === "rember")
-        return await channel.send("Hello I'm the Bedtime Bot and I remember it so you don't have to!");
+        return await channel.send({content: "Hello I'm the Bedtime Bot and I remember it so you don't have to!", tts: tts});
 
     if (response === "spotify")
         return await channel.send("https://open.spotify.com/track/5sICkBXVmaCQk5aISGR3x1?si=dc8aa47fd9774824");
+
+    if (response === "bedoclock")
+        return await channel.send("https://cdn.discordapp.com/attachments/958369561307131965/959309869133991967/unknown.png");
+
+    if (response === "monopoly")
+        return await channel.send("https://cdn.discordapp.com/attachments/958369561307131965/959310167625850930/unknown.png");
+
+    if (response === "poem1")
+        return await channel.send({content: `to help you sleep, I will now sing you a lullaby
+
+        Analysing records
+        
+        Suitable song found
+        
+        Now singing Golden Slumbers by Thomas Dekker, dated 1603
+        
+        Golden slumbers kiss your eyes,
+        Smiles awake you when you rise;
+        Sleep, pretty wantons, do not cry,
+        And I will sing a lullaby,
+        Rock them, rock them, lullaby.
+        
+        Care is heavy, therefore sleep you,
+        You are care, and care must keep you;
+        Sleep, pretty wantons, do not cry,
+        And I will sing a lullaby,
+        Rock them, rock them, lullaby.`, tts: tts});
+
+    if (response === "poem2")
+        return await channel.send({content: `to help you sleep, I will now sing you a lullaby
+
+        Analysing records
+        
+        Suitable song found
+        
+        Now singing Cradle Song by William Blake, dated 1789
+        
+        Sleep, sleep, beauty bright,
+        Dreaming in the joys of night;
+        Sleep, sleep; in thy sleep
+        Little sorrows sit and weep.
+        
+        Sweet babe, in thy face
+        Soft desires I can trace,
+        Secret joys and secret smiles,
+        Little pretty infant wiles.
+        
+        As thy softest limbs I feel
+        Smiles as of the morning steal
+        O’er thy cheek, and o’er thy breast
+        Where thy little heart doth rest.
+        
+        O the cunning wiles that creep
+        In thy little heart asleep!
+        When thy little heart doth wake,
+        Then the dreadful night shall break.`, tts: tts});
 
     assertNever(response);
 }
@@ -213,7 +271,7 @@ client.on("interactionCreate", async interaction => {
     } else interaction.reply({content: "This service is not set to receive private messages.", ephemeral: true});
 });
 
-schedule('0 22 * * *', async () => {
+schedule('0 2 * * *', async () => {
     findChannels();
 });
 
